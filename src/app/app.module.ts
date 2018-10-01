adfabfd92ba697e7c4c2f24bf5ce13d53742ab7d
Prabhu Kencharaddi <p.kencharaddi@elsevier.com>
2018-10-01 10:15:13 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { CampaignComponent } from './campaign/campaign.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppHeaderComponent,
+    CampaignComponent
+  ],
+  imports: [
+    BrowserModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
