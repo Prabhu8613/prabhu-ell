eb0724aa0731316c0bd86620b1b5a77782f2adb9
Prabhu Kencharaddi <p.kencharaddi@elsevier.com>
2018-10-03 10:42:07 +0100

    Added Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,35 @@ import { AppHeaderComponent } from './app-header/app-header.component';
 import { CampaignComponent } from './campaign/campaign.component';
 import { UserService } from './campaign/user-service';
 import { HttpModule } from '@angular/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { CustomerComponent } from './customer/customer.component';
+import { EmailComponent } from './email/email.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'campaign', component: CampaignComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'customer', component: CustomerComponent },
+  { path: 'email', component: EmailComponent },
+  { path: '', component: DashboardComponent }  
+];
 @NgModule({
   declarations: [
     AppComponent,
     AppHeaderComponent,
-    CampaignComponent
+    CampaignComponent,
+    DashboardComponent,
+    ContactsComponent,
+    CustomerComponent,
+    EmailComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
-    
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
